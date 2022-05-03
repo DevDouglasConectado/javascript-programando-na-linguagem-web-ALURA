236021728cc5696f83d89f0363b8d83ad7a2e2ab
Douglas <douglasrodriguesconectadoifmg@gmail.com>
2022-05-02 23:09:14 -0300

    codigos praticado em aula

--- introducao-javascript-inicial/js/principal.js
+++ introducao-javascript-inicial/js/principal.js
@@ -1,3 +1,45 @@
-console.log("fiu carregado de um arquivo externo");
+/*console.log("fiu carregado de um arquivo externo");*/
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida nutricionista";
+
+var pacinte = document.querySelector('#primeiro-paciente');
+
+var tdPeso = pacinte.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = pacinte.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+/*console.log(pacinte);
+
+console.log(tdPeso);
+console.log(peso);
+
+console.log(tdAltura);
+console.log(altura);
+console.log(imc);*/
+
+var tdImc = pacinte.querySelector(".info-imc");
+/*var imc = peso / (altura * altura);
+tdImc.textContent = imc;*/
+
+var pesoEhValido  =true;
+var alturaEhValida = true;
+
+
+if (peso <= 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
